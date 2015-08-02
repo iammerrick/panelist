c8daf3e8820d8aa67eb34e5597cc4ee53c9a9102
Merrick Christensen <merrick.christensen@gmail.com>
2015-08-02 16:51:58 -0400

    read for submit

--- app/AppHandler/CreateMessage/CreateMessage.js
+++ app/AppHandler/CreateMessage/CreateMessage.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class CreateMessage extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.state.value);  
+  }
+
   render() {
     return (
       <div>
-        <textarea />
+        <textarea onChange={this.handleChange.bind(this)} value={this.state.value} />
+        <a href="#" onClick={this.handleSubmit.bind(this)}>Submit</a>
       </div>
     );
   }
