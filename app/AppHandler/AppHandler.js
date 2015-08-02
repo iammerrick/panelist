5b97485ee8f77f7697ced74fd5adad9300eaa95c
Merrick Christensen <merrick.christensen@gmail.com>
2015-08-02 16:48:15 -0400

    Create message

--- app/AppHandler/AppHandler.js
+++ app/AppHandler/AppHandler.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ChatList from './ChatList/ChatList';
+import CreateMessage from './CreateMessage/CreateMessage';
 
 export default class AppHandler extends React.Component {
   render() {
-    return <ChatList discussion={this.props.store.discussion} />;
+    return (
+      <div>
+        <ChatList discussion={this.props.store.discussion} />
+        <CreateMessage />
+      </div>
+    );
   }
 }
 
