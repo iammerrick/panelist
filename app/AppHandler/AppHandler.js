681576f4fa161d79286acdb22d756e8d892213e3
Merrick Christensen <merrick.christensen@gmail.com>
2015-08-02 16:13:42 -0400

    react rendering app

---
+++ app/AppHandler/AppHandler.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class AppHandler extends React.Component {
+  render() {
+    return <div>Hello world!</div>;
+  }
+}
+
+
--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
-var React = require('react');
+import React from 'react';
 var Firebase = require('../universal/utils/FirebaseUtils').panel(ENV.PANEL);
+import AppHandler from './AppHandler/AppHandler';
+
+React.render(<AppHandler />, document.getElementById('app'));
 
 
