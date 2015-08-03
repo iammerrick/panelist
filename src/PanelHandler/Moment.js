415263bcf40aeb5aae41935f476ccb1a15177955
hossman333 <josh.robertson@domo.com>
2015-08-03 01:59:03 -0400

    made timestamp change if its different day

--- src/PanelHandler/Moment.js
+++ src/PanelHandler/Moment.js
@@ -3,7 +3,11 @@ import moment from 'moment';
 
 class Moment extends React.Component {
   render() {
-    var label = moment(this.props.timeStamp).format('MMMM Do YYYY, h:mm a');
+    if(moment(this.props.timeStamp).isSame(moment(), 'day')) {
+      var label = moment(this.props.timeStamp).format('h:mm a');
+    } else {
+      var label = moment(this.props.timeStamp).format('MMMM Do YYYY, h:mm a');
+    }
     return <span {...this.props}>{label}</span>
   }
 }
