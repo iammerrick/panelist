948b39dcb7e058a47154742285326d9e8ddf711c
hossman333 <josh.robertson@domo.com>
2015-08-04 20:15:53 -0400

    some icon styles

--- src/AppHandler/NavigationBar.js
+++ src/AppHandler/NavigationBar.js
@@ -5,6 +5,21 @@ class NavigationBar extends React.Component {
   render() {
     return (
       <div className='NavigationBar'>
+        <div className='NavigationBar__Icon'>
+          <a href='/panel/create' className='NavigationBar__PlusIcon'>
+            <i className='icon-plus'></i>
+          </a>
+        </div>
+        <div className='NavigationBar__Icon'>
+          <a href='/dashboard' className='NavigationBar__ListIcon'>
+            <i className='icon-list'></i>
+          </a>
+        </div>
+        <div className='NavigationBar__Icon'>
+          <a href='/logout' className='NavigationBar__LogoutIcon'>
+            <i className='icon-logout'></i>
+          </a>
+        </div>
       </div>
     );
   }
